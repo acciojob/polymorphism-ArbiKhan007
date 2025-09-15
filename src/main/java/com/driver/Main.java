15385bb43cfe08b04e8d27ca2acac3c158308955
Shamsuzzoha1 <c.shamsuz.zoha@generalaeronautics.com>
2025-09-15 14:46:03 +0530

    Making Product class inner for Main class

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,23 +1,24 @@
 package com.driver;
 
 public class Main {
+    static class Product{
+        public int product(int x, int y) {
+            return  x*y;
+        }
+
+        public int product(int x, int y, int z) {
+            return x*y*z;
+        }
+
+        public double product(double x, double y) {
+            return x*y;
+        }
+    }
+
     public static void main(String[] args) {
-        Product p=new Product();
+        Product p= new Product();
         int prod=p.product(1,2, 3);
         System.out.println(prod);
     }
 }
 
-class Product{
-    public int product(int x, int y) {
-        return  x*y;
-    }
-
-    public int product(int x, int y, int z) {
-        return x*y*z;
-    }
-
-    public double product(double x, double y) {
-        return x*y;
-    }
-}
